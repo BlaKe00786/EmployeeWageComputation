4a74b42b2f1ceab7ef59477325336ad98f27352f
BlaKe00786 <sameershaikh403@gmail.com>
2020-09-30 22:29:10 +0530

    [Sameer]Refactor:added funcionality to calculate wages for multiple companies

---
+++ EmployeeWages/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dmart.computeEmpWage();
+            Console.WriteLine(dmart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}
