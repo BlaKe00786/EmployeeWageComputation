2e09b69a09db11e27ab142525b52ca2ef45b02a6
BlaKe00786 <sameershaikh403@gmail.com>
2020-09-30 23:01:09 +0530

    [Sameer]Refactor: Added wages of Multiple Companies.

--- EmployeeWages/Class1.cs
+++ EmployeeWages/Class1.cs
@@ -4,16 +4,14 @@ using System.Text;
 
 namespace EmployeeWages
 {
-    class Class1
+    public class Class1
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dmart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
-            dmart.computeEmpWage();
-            Console.WriteLine(dmart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
